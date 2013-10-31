ff198799f530ed11031c1a946c7ded087a25dffb
ducnghiemminh <ducitbk@gmail.com>
2013-10-31 08:50:18 +0700

    Update ownego.sql

--- ownego.sql
+++ ownego.sql
@@ -1,23 +1,23 @@
-CREATE TABLE IF NOT EXISTS `deparment` (
-	`id`				INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `department` (
+	`id`			INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	`name`			VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `regency` (
-	`id`				INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	`id`			INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	`name`			VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `employee` (
-	`id`				INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
+	`id`			INT UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 	`name`			VARCHAR(50) NOT NULL,
-	`age`				INT UNSIGNED,
+	`age`			INT UNSIGNED,
 	`phone`			VARCHAR(12),
-	`gender`			TINYINT,
-	`date_of_birth`	DATE,
-	`family`			TEXT,
-	`department_id`	INT UNSIGNED,
+	`gender`		TINYINT,
+	`date_of_birth`		DATE,
+	`family`		TEXT,
+	`department_id`		INT UNSIGNED,
 	`regency_id`		INT UNSIGNED,
-	FOREIGN KEY (`department_id`) REFERENCES deparment(`id`) ON DELETE CASCADE,
+	FOREIGN KEY (`department_id`) REFERENCES department(`id`) ON DELETE CASCADE,
 	FOREIGN KEY (`regency_id`) REFERENCES regency(`id`) ON DELETE CASCADE
 );
